f7e18f7ca684bdaae12b1898667d19ca88e3d71d
Andrew Kriz <aakriz@gmail.com>
2014-10-25 02:35:36 -0400

    Re-added 30min loiter and 5% additional fuel for reserve and 1% additional fuel for trapped fuel

--- wftransport.m
+++ wftransport.m
@@ -6,7 +6,12 @@ function wffuel = wftransport(range,sfc_cruise,Vcruise,LDcruise)
     wfdescent=0.999;
 
     wfcruise=exp(1).^-(range.*sfc_cruise./(Vcruise.*LDcruise)); %for range
-    wfdivert=exp(1).^-(Vcruise.*sfc_cruise./(Vcruise.*LDcruise)); %for range
+    wfdivert=exp(1).^-(Vcruise.*sfc_cruise./(Vcruise.*LDcruise)); %for 1 hour at cruise
+    
+    loiter = 0.5; %hrs for tooling around in pattern/approach
+    wfloiter=exp(1).^-(loiter.*sfc_cruise./(Vcruise.*LDcruise)); %for endurance
 
-    wfmission=wfto.*wfclimb.*wfcruise.*wfdescent.*wfdivert.*wflanding;
-    wffuel=1.00*(1-wfmission);
+    wfmission=wfto.*wfclimb.*wfcruise.*wfdescent.*wfloiter.*wfdivert.*wflanding;
+    reserve = 0.05;
+    trapped = 0.01;
+    wffuel=(1+reserve+trapped)*(1-wfmission);
