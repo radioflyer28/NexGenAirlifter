09ce88a6cde5caaf0d6ea20b44ed7fb2f5c5c489
Andrew Kriz <aakriz@gmail.com>
2014-10-25 02:42:53 -0400

    Changed LD scaling constant from 0.943 to 0.866. Eric pointed out that 0.943 is used for cruise climb and 0.866 is used for level cruise...

--- LDcruise.m
+++ LDcruise.m
@@ -1,3 +1,3 @@
 function LD = LDcruise(Cd0,AR,e)
 % a crude function that estimates cruise L/D (from our design book)
-LD = 0.943 * 1./(4.*Cd0./(pi.*AR.*e)).^0.5;
+LD = 0.866 * 1./(4.*Cd0./(pi.*AR.*e)).^0.5;
