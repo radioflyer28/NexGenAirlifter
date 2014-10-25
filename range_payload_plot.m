4299cb04bdb76cb0b5535faaf2d3accc6b6e63f8
Andrew Kriz <aakriz@gmail.com>
2014-10-25 02:41:51 -0400

    Added oswald efficiency estimation based on wing geometry

--- range_payload_plot.m
+++ range_payload_plot.m
@@ -1,19 +1,29 @@
-function [] = range_payload()
-% function [] = range_payload(e,AR,Vcruise,sfc_cruise,Cd0)
+%function [] = range_payload_plot()
+clear all; clc; close all;
 
-e=0.52;
+fuse_wing_ratio = 0.114; %ratio of fuselage diameter to wingspan, 0.114 applies to most transport aircraft
+sweep_qtr_chord = 25; %degrees
+taper_ratio_opt = 0.45.*exp(1).^(-0.0375.*sweep_qtr_chord);
+dihedral = 5; %degrees
 AR=7.8;
-Vcruise=442; %knots from .77mach @ 37000ft
 Cd0=0.02;
-sfc_cruise_s=0.6;
+cruise_alt = 37000; %ft
+cruise_mach = 0.77;
+sfc_cruise_s=0.5;
+[rho,a,~,~,~,~]=stdatmo(cruise_alt,0,'US',true); %rho = slugs/cuft, a = ft/s
+Vcruise=cruise_mach*a*3600/6076; %knots
+
+e=oswald(fuse_wing_ratio,AR,taper_ratio_opt,sweep_qtr_chord,dihedral,cruise_mach,Cd0);
+% e=0.52; %optional override
 
 LD=LDcruise(Cd0,AR,e);
-% LD=20;
+% LD=20; %optional override
 
+s = warning('off', 'all'); %turn all warnings off
 mtow_guess=0;
 num=20;
 min_mtow=0;
-max_mtow=1.4e6;
+max_mtow=1.6e6;
 [w_payload,range]=meshgrid(linspace(0,600000,num),linspace(1000,8000,num));
 exitflag=-4;
 for i=1:num
@@ -21,9 +31,11 @@ for i=1:num
     for j=1:num
         j;
         wffuel(i,j)=wftransport(range(i,j),sfc_cruise_s,Vcruise,LD);
-        [w_mtow(i,j) fval exitflag output]=mtow_solve(wffuel(i,j),w_payload(i,j),min_mtow,max_mtow);
+        [w_mtow(i,j),~,exitflag,~]=mtow_solve(wffuel(i,j),w_payload(i,j),min_mtow,max_mtow);
+        %[w_mtow(i,j),fval,exitflag,output]
     end
 end
+warning(s)
 
 figure
 contour(range,w_payload,w_mtow,[8e5:2e5:max_mtow],'ShowText','on')
